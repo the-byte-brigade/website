16abd45688237edb6956056489952d2a05638e87
sanju2728 <sanjanabhat2002@gmail.com>
2023-04-25 18:52:26 +0530

    ui: added reporting form

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,13 +1,7 @@
 <div class="my-20">
     <form class="max-w-md mx-auto mt-8 space-y-6 mt-auto my-10" action="http://127.0.0.1:5000/submit" method="post">
       <div>
-        <label class="block font-medium text-gray-700 text-left" for="first-name">First Name</label>
-        <input name="first-name" class="text-black form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm" id="first-name" type="text">
-      </div>
-      <div>
-        <label class="block font-medium text-gray-700 text-left" for="last-name">Last Name</label>
-        <input name="last-name" class="text-black form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm" id="last-name" type="text">
-      </div>
+       
       
       <div>
         <label class="block font-medium text-gray-700 text-left" for="address">Location of the animal</label>
@@ -29,49 +23,28 @@
       </div>
       
   
-      <label class="block font-medium text-gray-700" for="animal-preference">Animal preference</label>
+      <label class="block font-medium text-gray-700" for="animal-preference">What animal did you find?</label>
       <select name="animal-preference" class="text-black form-select mt-1 block w-full rounded-md border-gray-300 shadow-sm" id="animal-preference">
-        <option value="">What animal did you find?</option>
+        <option value="">Choose here</option>
         <option value="dog">Dog</option>
         <option value="cat">Cat</option>
         <option value="bird">Bird</option>
         <option value="other">Other</option>
       </select>
+
     
     <div class="mt-4">
-      <label class="block font-medium text-gray-700" for="color">Color</label>
-      <input name="color" class="text-black form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm" id="color" type="text">
-    </div>
-    <div class="mt-4">
-      <label class="block font-medium text-gray-700" for="gender">Gender</label>
-      <select name="gender" class="text-black form-select mt-1 block w-full rounded-md border-gray-300 shadow-sm" id="gender">
-        <option value="">Choose a gender</option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="unknown">Unknown</option>
-      </select>
-    </div>
-    <div class="mt-4">
-      <label class="block font-medium text-gray-700" for="breed">Breed</label>
-      <input name="breed" class="text-black form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm" id="breed" type="text">
-    </div>
-    <div class="mt-4">
-      <label class="block font-medium text-gray-700">Ownership commitment</label>
-      <div class="mt-2">
-        <label class="inline-flex items-center">
-          <input type="checkbox" class="text-black form-checkbox" name="follow-up-reports">
-          <span class="ml-2">Do you agree to give us follow-up reports and photos from time to time?</span>
-        </label>
+        <label class="block font-medium text-gray-700" for="gender">Mode of danger</label>
+        <select name="danger" class="text-black form-select mt-1 block w-full rounded-md border-gray-300 shadow-sm" id="danger">
+          <option value="male">Choose here</option>
+          <option value="male">Abandoned</option>
+          <option value="female">Sick</option>
+          <option value="unknown">Abuse</option>
+        </select>
       </div>
-      <div class="mt-2">
-        <label class="inline-flex items-center">
-          <input type="checkbox" class="text-black form-checkbox" name="re-home-pet">
-          <span class="ml-2">Do you agree to contact us in case you need to re-home this pet for whatever reason?</span>
-        </label>
-      </div>
-    </div>
+    
     <div class="mt-4">
-      <label class="block font-medium text-gray-700" for="other-questions">Do you have any other questions for us you’d like us to note?</label>
+      <label class="block font-medium text-gray-700" for="other-questions">Give us more details</label>
       <textarea class="text-black form-textarea mt-1 block w-full rounded-md border-gray-300 shadow-sm" id="other-questions" rows="3"></textarea>
     </div>
     <div class="mt-6">
